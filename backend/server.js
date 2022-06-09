9b08c72136ad7b35058c4907f0bd4bff519ac0ab
mmislam <mmislam272@gmail.com>
2022-06-10 02:17:33 +0600

    Nodemon setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ const app = express()
 
 app.get('/',(req,res)=>{res.send("Hi")})
 
-const port=process.env.PORT || 5000;
+const port=process.env.PORT || 5000; 
 
 app.listen(port,()=>{console.log(`Serving at localhost:${port}`)})
