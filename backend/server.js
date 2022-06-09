88ee34e3fcca52147812f921170ab7c63e192953
mmislam <mmislam272@gmail.com>
2022-06-09 21:52:43 +0600

    New start

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express()
+
+app.get('/',(req,res)=>{res.send("Hi")})
+
+const port=process.env.PORT || 5000;
+
+app.listen(port,()=>{console.log(`Serving at localhost:${port}`)})
