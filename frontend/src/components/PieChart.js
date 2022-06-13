e31fb4783eb70aac5187399169b8553c55e3a6ec
mmislam <mmislam272@gmail.com>
2022-06-13 22:51:21 +0600

    Smart pie chart the beginning.

---
+++ frontend/src/components/PieChart.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PieChart = () => {
+  return (
+    <div className="pieChartBox">
+        <div className='piechart'>
+            <div className='pieChartInnerCircle'>
+                <div className='pieChartTextBox'>
+                  <h1 className='pieChartText'></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PieChart
