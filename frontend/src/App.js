6410b0029cf5d298119e044c5ab5253ab6162ae1
mmislam <mmislam272@gmail.com>
2022-06-21 21:51:37 +0600

    three pie done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import PieChart from "./components/PieChart.js";
 import "./index.css";
 
+
+
 function App() {
+  
+
+
+
   return (
     <div className="App">
-      
+      <PieChart ></PieChart>
     </div>
   );
 }
--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import{ useState}  from 'react'
+
 
 const PieChart = () => {
+  
+  const [colors, setColors]=useState({redFrom: "0", redTo:"20",greenFrom : "20", greenTo: "70", blueFrom: "70", blueTo: "100"})
+
   return (
     <div className="pieChartBox">
-        <div className='piechart'>
+        <div className='pieChart' style={{backgroundImage: `conic-gradient(red ${colors.redFrom}%,red ${colors.redTo}%, green ${colors.greenFrom}%,green ${colors.greenTo}%,blue ${colors.blueFrom}%, blue  ${colors.blueTo}% )`}}>
             <div className='pieChartInnerCircle'>
                 <div className='pieChartTextBox'>
-                  <h1 className='pieChartText'></h1>
+                  <h1 className='pieChartText'>{}</h1>
+                </div>
+            </div>
+        </div>
+
+        <div className='pieChart' style={{backgroundImage: `conic-gradient(red ${colors.redFrom}%,red ${colors.redTo}%, green ${colors.greenFrom}%,green ${colors.greenTo}%,blue ${colors.blueFrom}%, blue  ${colors.blueTo}% )`}}>
+            <div className='pieChartInnerCircle'>
+                <div className='pieChartTextBox'>
+                  <h1 className='pieChartText'>{}</h1>
+                </div>
+            </div>
+        </div>
+
+        <div className='pieChart' style={{backgroundImage: `conic-gradient(red ${colors.redFrom}%,red ${colors.redTo}%, green ${colors.greenFrom}%,green ${colors.greenTo}%,blue ${colors.blueFrom}%, blue  ${colors.blueTo}% )`}}>
+            <div className='pieChartInnerCircle'>
+                <div className='pieChartTextBox'>
+                  <h1 className='pieChartText'>{}</h1>
                 </div>
             </div>
         </div>
