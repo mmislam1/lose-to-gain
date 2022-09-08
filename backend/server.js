82dc85a5c564d80095709d384f70c048c9430b53
mmislam <mmislam272@gmail.com>
2022-09-08 23:42:14 +0600

    signin-registration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-
+import mongoose from 'mongoose'
 const app = express()
+mongoose.connect('mongodb://localhost/amazona')
+app.get('/', (req, res) => { res.send("Hi") })
 
-app.get('/',(req,res)=>{res.send("Hi")})
+const port = process.env.PORT || 5000;
 
-const port=process.env.PORT || 5000; 
-
-app.listen(port,()=>{console.log(`Serving at localhost:${port}`)})
+app.listen(port, () => { console.log(`Serving at localhost:${port}`) })
