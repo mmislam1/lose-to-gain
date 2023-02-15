481d61a08813d261c60f6061b4cddf614d45be2a
mmislam <mmislam272@gmail.com>
2023-02-15 10:26:07 +0600

    Authorization

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 import express from "express";
 import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost/amazona')
 app.get('/', (req, res) => { res.send("Hi") })
 
