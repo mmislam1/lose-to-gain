9a5b42fd67eec520fd941d83da16b97e0e83c578
mmislam <mmislam272@gmail.com>
2023-02-15 10:22:19 +0600

    Authorization

---
+++ frontend/src/components/FoodList/FoodList.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const FoodList = () => {
+
+  const foodItems = useSelector(state => state.foodItems)
+
+
+  return (
+    <div>FoodList</div>
+  )
+}
+
+export default FoodList
