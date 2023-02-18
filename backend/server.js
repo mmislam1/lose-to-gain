e28ae7623760f045337808f7286accf1da410ba7
mmislam <mmislam272@gmail.com>
2023-02-19 00:23:20 +0600

    connected

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 import express from "express";
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
+import userRouter from './routers/userRouter.js'
 
 dotenv.config();
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const conn = process.env.DATABASE_URL
 
-mongoose.connect('mongodb://localhost/amazona')
-app.get('/', (req, res) => { res.send("Hi") })
+mongoose.connect(conn)
 
-const port = process.env.PORT || 5000;
+const database = mongoose.connection
+
+// routers ...
+
+app.use('/api/users', userRouter)
+
+
+
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => { console.log(`Serving at localhost:${port}`) })
